238c924c92a69e0b1b94b12b986afa78cc0779f0
muhammedmmtnr <muhammedmmtnr@gmail.com>
2025-02-13 21:05:11 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/chiptabbar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chiptabbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/imageclass.dart
@@ -0,0 +1,8 @@
+class ImageClass {
+  static const String imageRoot = 'assets/images/';
+
+  static const String bgmayyith = '${imageRoot}mayyith image.jpg';
+  static const String bgjanza = '${imageRoot}janza image.jpg';
+  static const String applogo = '${imageRoot}app logo.png';
+
+}
